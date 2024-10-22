c8b7a57766490040bf655e8182d36b782cf6830f
leyrefernandezasensio <leyre.fernandez01@estudiant.upf.edu>
2024-10-22 10:16:48 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -2,15 +2,29 @@
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
 def main() :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+	i = 1
+r = 0
+x = int(input("Enter one value: "))
 
+while i <= x:
+    if x % i == 0:
+        r = r + i
+    i = i + 1
+l = x - 1
+s = 0
+while l >= 1 and s < r:
+    a = 1
+    s = 0
+    while a <= l:
+        if l % a == 0:
+            s = s + a
+        a = a + 1
+    l = l - 1
+if s > r:
+    print("not antiprime")
+else:
+    print("antiprime")
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
 	return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
