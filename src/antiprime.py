162f88c8966aa526cd6640642231f7aca4f1f043
leyrefernandezasensio <leyre.fernandez01@estudiant.upf.edu>
2024-10-22 11:07:26 +0200

    Update antiprime.py

--- src/antiprime.py
+++ src/antiprime.py
@@ -2,32 +2,32 @@
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
 import sys
-
-def main(n) :
-    d = 0
-    i = 1
-    while i <= n:
-        if n % i == 0:
-            d += 1
-        i += 1
-    return d
-
-def ap(n):
-    num_divisores = main(n)
-    i = 1
-    while i < n:
-        if main(i) >= num_divisores:
-            return "not anti-prime"
-        i += 1
-    return "anti-prime"  
-
-if _name_ == "_main_":
-    if len(sys.argv) != 2:
-        print("Uso: python antiprime.py <entero-positivo>")
-    else:
-        num = int(sys.argv[1])
-        res = ap(num)
-        print(res)
+    
+    def main(n) :
+        d = 0
+        i = 1
+        while i <= n:
+            if n % i == 0:
+                d += 1
+            i += 1
+        return d
+    
+    def ap(n):
+        num_divisores = main(n)
+        i = 1
+        while i < n:
+            if main(i) >= num_divisores:
+                return "not anti-prime"
+            i += 1
+        return "anti-prime"  
+    
+    if _name_ == "_main_":
+        if len(sys.argv) != 2:
+            print("Uso: python antiprime.py <entero-positivo>")
+        else:
+            num = int(sys.argv[1])
+            res = ap(num)
+            print(res)
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
